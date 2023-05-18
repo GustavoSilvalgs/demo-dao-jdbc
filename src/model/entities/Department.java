7dbf1981950eedbd616bc4411fd234a320b48e93
GustavoSilvalgs <luisgust06@gmail.com>
2023-05-18 17:34:00 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -49,6 +49,9 @@ public class Department implements Serializable {
 
     @Override
     public String toString() {
-        return "id = " + id +  ", " + "name = "  + name;
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
